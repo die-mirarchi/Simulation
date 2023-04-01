0422159e6799d424c86f0ccffc0797287866d910
die-mirarchi <diemirarchi@gmail.com>
2023-04-01 11:29:31 -0300

    Refactoring and draw funtion

---
+++ cmake-build-debug/CMakeFiles/Simulation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simulation.dir/main.c.o"
+  "CMakeFiles/Simulation.dir/main.c.o.d"
+  "Simulation"
+  "Simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,9 +1,58 @@
-#include "wave_fun.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
+#define BOARD_SIZE 10
+
 // " " - "╗" - "╝" - "╚" - "╔"
+const wint_t tiles[] = {0x0020, 0x2557,0x255D,0x255A,0x2554};
+
+typedef struct {
+    int collapse;
+    int options[5];
+} cell;
 
 
+void init_board(cell b[BOARD_SIZE][BOARD_SIZE]);
+int pick_random(int const arr[]);
+void draw_board(cell b[BOARD_SIZE][BOARD_SIZE]);
 int main() {
-    printf("%c\n",ch[1]);
+    setlocale(LC_CTYPE, "");
+    srand(time(NULL));
+    cell board[BOARD_SIZE][BOARD_SIZE];
+    init_board(board);
+    draw_board(board);
 
     return 0;
 }
+
+void init_board(cell b[BOARD_SIZE][BOARD_SIZE]){
+    for (int i = 0;i < BOARD_SIZE;  i++) {
+        for (int j = 0;j < BOARD_SIZE;j++) {
+            b[i][j].collapse = 0;
+            for (int k = 0; k < 5; ++k) {
+                b[i][j].options[k] = k;
+            }
+        }
+    }
+}
+int pick_random(int const arr[]){
+    int r = rand() % 5;
+    while (1){
+        if (arr[r] < 0){
+            r=rand()%5;
+        } else{
+            return arr[r];
+        }
+    }
+}
+void draw_board(cell b[BOARD_SIZE][BOARD_SIZE]){
+    for (int i = 0;i < BOARD_SIZE;  i++) {
+        for (int j = 0;j < BOARD_SIZE;j++) {
+            printf("%lc",tiles[pick_random(b[i][j].options)]);
+        }
+        printf("\n");
+    }
+}
+
